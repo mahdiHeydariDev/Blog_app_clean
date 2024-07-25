f1728a1314b014d0a15d112f212795aa8d69c81c
mahdi heydari <mahdi.heydari.developer@gmail.com>
2024-07-26 00:13:36 +0330

    Feat : Add custom colors.

--- lib/config/theme/pallet_color.dart
+++ lib/config/theme/pallet_color.dart
@@ -5,6 +5,7 @@ class PalletColor {
   static const Color gradiant1 = Color.fromRGBO(187, 63, 221, 1);
   static const Color gradiant2 = Color.fromRGBO(251, 109, 169, 1);
   static const Color gradiant3 = Color.fromRGBO(255, 159, 124, 1);
+  static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
   static const Color whiteColor = Colors.white;
   static const Color greyColor = Colors.grey;
   static const Color errorColor = Colors.redAccent;
