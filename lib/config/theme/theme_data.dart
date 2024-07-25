256f51cbd6f84f8eecc1bd9fe5d2befb0ff882d0
mahdi heydari <mahdi.heydari.developer@gmail.com>
2024-07-26 00:14:24 +0330

    Feat : Add custom Theme

--- lib/config/theme/theme_data.dart
+++ lib/config/theme/theme_data.dart
@@ -2,8 +2,26 @@ import 'package:blog_app_clean/config/theme/pallet_color.dart';
 import 'package:flutter/material.dart';
 
 class AppThemeData {
+  static OutlineInputBorder border({Color color = PalletColor.borderColor}) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide(
+        color: color,
+        width: 3,
+      ),
+    );
+  }
+
   static ThemeData themeData = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: PalletColor.scafFoldBackGround,
-    inputDecorationTheme: const InputDecorationTheme(),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(27),
+      border: border(),
+      enabledBorder: border(),
+      focusedBorder: border(color: PalletColor.gradiant2),
+      errorBorder: border(color: PalletColor.errorColor),
+      iconColor: PalletColor.gradiant2,
+      prefixIconColor: PalletColor.gradiant2,
+    ),
   );
 }
