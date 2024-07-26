87e02b8ea96acda49073c88da825e315572031e6
mahdi heydari <mahdi.heydari.developer@gmail.com>
2024-07-26 18:02:55 +0330

    Refactor : Add init supa base

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:blog_app_clean/config/theme/theme_data.dart';
-import 'package:blog_app_clean/features/auth/presentation/screens/signup_screen.dart';
+import 'package:blog_app_clean/core/constaints/url_constants.dart';
+import 'package:blog_app_clean/features/auth/presentation/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ignore: unused_local_variable
+  final Supabase supabase = await Supabase.initialize(
+    url: UrlConstants.baseUrl,
+    anonKey: UrlConstants.apiKey,
+  );
   runApp(
     const MyApp(),
   );
@@ -16,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppThemeData.themeData,
-      home: const SignupScreen(),
+      home: const SigninScreen(),
     );
   }
 }
